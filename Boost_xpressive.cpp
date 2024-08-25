587eff7089e08fd795f0bf8193f520ac4351a48d
Andrey-78 <avk11121978@mail.ru>
2024-08-25 15:24:08 +0300

    Добавьте файлы проекта.

---
+++ Boost_xpressive.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <boost/xpressive/xpressive.hpp>
+
+using namespace boost::xpressive;
+using namespace std;
+
+int main() {
+
+   string card_number = "1111-2222 3333-4444";
+   sregex card_regex = sregex::compile("\\d{4}[- ]\\d{4}[- ]\\d{4}[- ]\\d{4}");
+
+   if (regex_match(card_number, card_regex)) {
+      cout << "Success" << endl;
+   }
+   else {
+      cout << "Bad credit card number" << endl;
+   }
+
+   return 0;
+}
